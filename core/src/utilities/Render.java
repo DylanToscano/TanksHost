b7f8ce72f8a6027e7ecf6e4eddfdd97942696b39
SirTosky <dylantoscano.arg@gmail.com>
2021-11-30 16:46:37 -0300

    Update Render.java

--- core/src/utilities/Render.java
+++ core/src/utilities/Render.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.World;
 import com.bws.tankshost.TanksHost;
@@ -16,8 +15,8 @@ import elements.Explosion;
 import elements.Projectile;
 import elements.Tank;
 import elements.Updateable;
+import network.ServerClient;
 import network.ServersideThread;
-
 
 public abstract class Render {
 	public static SpriteBatch batch;
@@ -28,85 +27,116 @@ public abstract class Render {
 	public static ArrayList<Updateable> updateList = new ArrayList<Updateable>();
 	static ServersideThread serversideThread;
 
-	public static void render(){ //Render everything in the renderList
+	public static void render() { // Render everything in the renderList
 		batch.begin();
-		for(int i=0; i<renderList.size(); i++){
-			if(renderList.get(i) != null) {
+		for (int i = 0; i < renderList.size(); i++) {
+			if (renderList.get(i) != null) {
 				renderList.get(i).draw(batch);
 				try {
-					//before it takes that the Sprite is intance of The class we want to ask, so it doesnt cast a buff as a projectile.
-					// here it takes if a projectile is exploded or a buff picked so  it disappear, cause
-					// we cant disappear it at the moment of the world listener cause the game crashes because the world2d is locked.
-				
-					if(renderList.get(i) instanceof Buff && ((Buff) renderList.get(i)).isPicked()) {
-						
-						((Buff) renderList.get(i)).disappear();;
+					// before it takes that the Sprite is intance of The class we want to ask, so it
+					// doesnt cast a buff as a projectile.
+					// here it takes if a projectile is exploded or a buff picked so it disappear,
+					// cause
+					// we cant disappear it at the moment of the world listener cause the game
+					// crashes because the world2d is locked.
+
+					if (renderList.get(i) instanceof Buff && ((Buff) renderList.get(i)).isPicked()) {
+
+						((Buff) renderList.get(i)).disappear();
+						;
 						renderList.remove(i);
-						
-					}else if(renderList.get(i) instanceof Projectile && ((Projectile) renderList.get(i)).isExploded()) {
+
+					} else if (renderList.get(i) instanceof Projectile
+							&& ((Projectile) renderList.get(i)).isExploded()) {
 						((Projectile) renderList.get(i)).disappear();
-						
+
 						renderList.remove(i);
-					}else if(renderList.get(i) instanceof Explosion && ((Explosion) renderList.get(i)).end) {
+					} else if (renderList.get(i) instanceof Explosion && ((Explosion) renderList.get(i)).end) {
 						renderList.remove(i);
-					}else if(renderList.get(i) instanceof BarrelEx && ((BarrelEx) renderList.get(i)).hit) {
+					} else if (renderList.get(i) instanceof BarrelEx && ((BarrelEx) renderList.get(i)).hit) {
 						((BarrelEx) renderList.get(i)).disappear();
 						renderList.remove(i);
 					}
-				}catch(Exception e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
-			}else  {
+			} else {
 				renderList.remove(i);
 			}
-			
+
 		}
 		batch.end();
 	}
-	public static void disposeList(){
-		for(int i=0; i<renderList.size(); i++){
-			if(renderList.get(i) != null) {
-				renderList.get(i).getTexture().dispose();;
-			}else {
+
+	public static void disposeList() {
+		for (int i = 0; i < renderList.size(); i++) {
+			if (renderList.get(i) != null) {
+				renderList.get(i).getTexture().dispose();
+				;
+			} else {
 				renderList.remove(i);
 			}
 		}
 	}
-	public static void updateList(){
-		for(int i=0; i<updateList.size(); i++){
-			if(updateList.get(i) != null) {
-				
+
+	public static void updateList() {
+		for (int i = 0; i < updateList.size(); i++) {
+			if (updateList.get(i) != null) {
+
 				try {
 					updateList.get(i).update();
-					if(((Explosion) updateList.get(i)).end) {
+					if (((Explosion) updateList.get(i)).end) {
 						updateList.remove(i);
 					}
-				}catch(Exception e) {
-					
+				} catch (Exception e) {
+
 				}
-				
-				
-			}else {
+
+			} else {
 				updateList.remove(i);
 			}
 		}
 	}
-	
+
 	public static void cleanScreen() {
 		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
-	
+
+	public static void setServerThread(ServersideThread thread) {
+		serversideThread = thread;
+	}
+
+	public static void addUpdateable(Updateable update) {
+		updateList.add(update);
+	}
+
 	public static void addSprite(ClientSprite sprite) {
 		sprite.setID(serversideThread.generateSpriteID());
 		renderList.add(sprite);
 		serversideThread.addSprite(sprite);
 	}
-	public static void addUpdateable(Updateable update) {
-		updateList.add(update);
+
+	public static void removeSprite() {
+
 	}
 	
-	public static void setServerThread(ServersideThread thread) {
-		serversideThread = thread;
+//////TANK MANAGEMENT
+	public static void createTank(ServerClient client) {
+		tanks.add(new Tank(client));
 	}
+	
+	public static void removeTank(ServerClient client) {
+		Tank removedTank = null;
+		for (int i = 0; i < tanks.size(); i++) {
+			if(tanks.get(i).owner == client) {
+				removedTank = tanks.get(i);
+				break;
+			}
+		}
+		if (removedTank != null){
+			
+		}
+	}
+
 }
